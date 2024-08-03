bd58fe861b6e1ab1d41939599b7fbc9f7d4bc16a
Vladislav Evg <linesgreen@mail.ru>
2024-08-03 20:25:03 +0300

    update prisma client for debian v2

--- apps/app/prisma/client/edge.js
+++ apps/app/prisma/client/edge.js
@@ -167,7 +167,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- apps/app/prisma/client/index.js
+++ apps/app/prisma/client/index.js
@@ -168,7 +168,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
