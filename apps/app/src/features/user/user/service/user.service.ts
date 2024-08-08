e3bbc79a2bd0a68d56ac7a8ccf7cfb294b4cebaf
Vladislav Evg <linesgreen@mail.ru>
2024-08-08 20:37:20 +0300

    (IN-19)fix: fix object result types

--- apps/app/src/features/user/user/service/user.service.ts
+++ apps/app/src/features/user/user/service/user.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from '../repository/user.repository';
+import { ObjResult } from '../../../../../../common/utils/result/object-result';
+import { BadRequestError } from '../../../../../../common/utils/result/custom-error';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
   async createUser(data: { data: any }) {
-    await this.userRepository.createUser(data);
+    if (!data.data.test) return ObjResult.Err(new BadRequestError('test is required', 'test'));
+    console.log(data);
+    const result = await this.userRepository.createUser(data);
+    return ObjResult.Ok(result);
   }
 
   async getAllUsers() {
--- apps/common/utils/result/object-result.ts
+++ apps/common/utils/result/object-result.ts
@@ -1,41 +1,51 @@
-import { CustomError } from './custom-error';
+import { BadRequestError, CustomError, ErrorStatus } from './custom-error';
 
-export class Result<T> {
+export class ObjResult<T> {
   private constructor(
     private readonly _isSuccess: boolean,
     private readonly _value?: T,
     private readonly _error?: CustomError,
   ) {}
 
-  public static Ok<U>(value?: U): Result<U> {
+  public static Ok<U>(value?: U): ObjResult<U> {
     let parsedValue: any = true;
 
     if (Boolean(value) || Number(value) === 0) parsedValue = value;
 
-    return new Result<U>(true, parsedValue as unknown as U);
+    return new ObjResult<U>(true, parsedValue as unknown as U);
   }
 
-  public static Err<U>(err: CustomError | string): Result<U> {
+  public static Err<U>(err: CustomError | string): ObjResult<U> {
     let error: CustomError = err as CustomError;
 
     if (typeof err === 'string') {
       error = new CustomError(err as string);
     }
 
-    return new Result<U>(false, null, error);
+    return new ObjResult<U>(false, null, error);
   }
 
   get value(): T {
-    if (!this._value && Number(this._value) !== 0) throw new Error('Does not extract value from Result');
+    if (!this._value && Number(this._value) !== 0) throw new Error('Does not extract value from ObjResult');
     return this._value as T;
   }
 
   get error(): CustomError {
-    if (!this._error) throw new Error('Does not extract error from Result');
+    if (!this._error) throw new Error('Does not extract error from ObjResult');
     return this._error;
   }
 
   get isSuccess(): boolean {
     return this._isSuccess;
+  }
+}
+
+export class ErrorResulter {
+  static proccesError<T>(error: CustomError): void {
+    console.log(error instanceof BadRequestError);
+    switch (true) {
+      case error instanceof BadRequestError:
+        throw error;
+    }
   }
 }
