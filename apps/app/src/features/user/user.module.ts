4c086a7b7435735a82de4ff4f2340dee28ed1ecf
Vladislav Evg <linesgreen@mail.ru>
2024-08-19 16:34:18 +0300

    (IN-25)fix: code review x3

--- apps/app/src/features/user/user.module.ts
+++ apps/app/src/features/user/user.module.ts
@@ -3,17 +3,17 @@ import { UserController } from './user/controller/user.controller';
 import { AuthController } from './auth/controller/auth.controller';
 import { UserService } from './user/service/user.service';
 import { UserRepository } from './user/repository/user.repository';
-import { RegistrationUserUseCase } from './auth/use.cases/registrarion.user.use.case';
-import { RegistrationEmailResendingUseCase } from './auth/use.cases/registration-email-resending.user.use.case';
-import { RegistrationConfirmationUseCase } from './auth/use.cases/registration-confirmation.user.use.case';
-import { LoginUserUseCase } from './auth/use.cases/login.user.use.case';
-import { LogoutUserUseCase } from './auth/use.cases/logout.user.use.case';
-import { PasswordRecoveryUseCase } from './auth/use.cases/password-recovery.user.use.case';
-import { SetNewPasswordUseCase } from './auth/use.cases/set-new-password.user.use.case';
 import { CqrsModule } from '@nestjs/cqrs';
 import { JwtModule } from '@nestjs/jwt';
 import { EmailAdapter } from './auth/email.adapter/email.adapter';
 import { PrismaModule } from '../../common/database_module/prisma.module';
+import { RegistrationUserUseCase } from './auth/command/registrarion.user.use.case';
+import { RegistrationEmailResendingUseCase } from './auth/command/registration-email-resending.user.use.case';
+import { RegistrationConfirmationUseCase } from './auth/command/registration-confirmation.user.use.case';
+import { LoginUserHandler } from './auth/command/login.user.command';
+import { LogoutUserUseCase } from './auth/command/logout.user.use.case';
+import { PasswordRecoveryUseCase } from './auth/command/password-recovery.user.use.case';
+import { SetNewPasswordUseCase } from './auth/command/set-new-password.user.use.case';
 
 const userCommands = [];
 const userRepositories = [UserRepository];
@@ -22,7 +22,7 @@ const useCases = [
   RegistrationUserUseCase,
   RegistrationEmailResendingUseCase,
   RegistrationConfirmationUseCase,
-  LoginUserUseCase,
+  LoginUserHandler,
   LogoutUserUseCase,
   PasswordRecoveryUseCase,
   SetNewPasswordUseCase,
