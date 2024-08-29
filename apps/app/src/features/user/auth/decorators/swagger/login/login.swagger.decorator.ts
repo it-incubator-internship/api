c0214bba9d83c3967ecf4be9981a3e748c739c10
AliakseiYarmolin <aliakseiyarmolinforit@gmail.com>
2024-08-29 16:31:56 +0300

    fix swagger

--- apps/app/src/features/user/auth/decorators/swagger/login/login.swagger.decorator.ts
+++ apps/app/src/features/user/auth/decorators/swagger/login/login.swagger.decorator.ts
@@ -8,15 +8,15 @@ import {
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 
-import { UserRegistrationOutputDto } from '../../../dto/output/registratio.output.dto';
 import { BasicBadRequestOutputType } from '../../../../../../common/models/basic-badquest.output.type';
 import { LoginUserInputModel } from '../../../dto/input/login.user.dto';
+import { UserLoginOutputDto } from '../../../dto/output/login.output.dto';
 
 export function LoginSwagger() {
   return applyDecorators(
     ApiOperation({ summary: 'Вход в систему' }),
     ApiBody({ type: () => LoginUserInputModel }),
-    ApiResponse({ status: 201, type: () => UserRegistrationOutputDto }),
+    ApiResponse({ status: 201, type: () => UserLoginOutputDto }),
     ApiBadRequestResponse({ status: 400, type: () => BasicBadRequestOutputType }),
     // TODO переговорить с фронтом по поводу редиректа
     ApiForbiddenResponse({ status: 403, description: 'в случае неподтверждённого аккаунта' }),
--- apps/app/src/features/user/auth/decorators/swagger/refresh-token/refresh-token.swagger.decorator.ts
+++ apps/app/src/features/user/auth/decorators/swagger/refresh-token/refresh-token.swagger.decorator.ts
@@ -1,10 +1,12 @@
 import { applyDecorators } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiSecurity, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
+import { UserLoginOutputDto } from '../../../dto/output/login.output.dto';
+
 export function RefreshTokenSwagger() {
   return applyDecorators(
     ApiOperation({ summary: 'Обновление токенов' }),
-    ApiResponse({ status: 201 }),
+    ApiResponse({ status: 201, type: () => UserLoginOutputDto }),
     ApiUnauthorizedResponse({ status: 401 }),
     ApiSecurity('refreshToken'),
   );
