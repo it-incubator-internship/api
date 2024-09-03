a6c8a8f120581cb82e3c0e3e19d82def31d54e50
Vladislav Evg <linesgreen@mail.ru>
2024-09-03 19:43:16 +0300

    (DEV)fix: fix cleaning.module.ts

--- apps/app/src/features/cleaning/cleaning.module.ts
+++ apps/app/src/features/cleaning/cleaning.module.ts
@@ -3,9 +3,12 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../common/database_module/prisma.module';
 
 import { CleaningController } from './controller/cleaning.controller';
+import { CleaningService } from './cleaning.service';
 
 @Module({
   imports: [PrismaModule],
+  providers: [CleaningService],
   controllers: [CleaningController],
+  exports: [CleaningService],
 })
 export class CleaningModule {}
