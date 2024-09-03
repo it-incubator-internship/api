5391e7dc54c9e67bade97dc0a266785fbb78a988
Vladislav Evg <linesgreen@mail.ru>
2024-09-03 19:17:22 +0300

    (IN-07)FEAT: add cleaning module, fix oauth test

--- apps/app/src/app.module.ts
+++ apps/app/src/app.module.ts
@@ -6,10 +6,10 @@ import { UserModule } from './features/user/user.module';
 import { configuration } from './common/settings/configuration';
 import { Environments } from './common/settings/env_validate/env-class-validator';
 import { getEnvFilePath, isEnvFileIgnored } from './common/settings/determinate-env-path';
-import { CleaningController } from './features/cleaning/controller/cleaning.controller';
 import { UserRepository } from './features/user/user/repository/user.repository';
 import { SessionRepository } from './features/user/auth/repository/session.repository';
 import { PrismaService } from './common/database_module/prisma-connection.service';
+import { CleaningModule } from './features/cleaning/cleaning.module';
 
 const environment = process.env.NODE_ENV as Environments;
 
@@ -24,8 +24,9 @@ const ybrat = [UserRepository, SessionRepository, PrismaService];
       load: [configuration],
     }),
     UserModule,
+    CleaningModule,
   ],
-  controllers: [AppController, CleaningController],
+  controllers: [AppController],
   providers: [...ybrat],
   exports: [],
 })
---
+++ apps/app/src/features/cleaning/cleaning.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from '../../common/database_module/prisma-connection.service';
+
+@Injectable()
+export class CleaningService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async cleanDB() {
+    await this.prismaService.session.deleteMany();
+    await this.prismaService.accountData.deleteMany();
+    await this.prismaService.user.deleteMany();
+  }
+}
