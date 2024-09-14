b2ab0b9ad920abfd10eef8fd11deb17116596b5c
Vladislav Evg <linesgreen@mail.ru>
2024-09-14 12:29:51 +0300

    (IN-77)FIX: fix rmq address

--- apps/payments/src/main.ts
+++ apps/payments/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://guest:guest@localhost:5672'],
+      urls: ['amqp://guest:guest@localhost:56722'],
       queue: 'payments_queue',
       queueOptions: {
         durable: false,
