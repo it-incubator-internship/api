d2cc4ff0e6ca6c46af77b3e79bcc01b2f6b4cfdd
Vladislav Evg <linesgreen@mail.ru>
2024-09-16 20:44:26 +0300

    (IN-92)FEAT: add delete user by id from testing

--- apps/app/src/features/cleaning/controller/cleaning.controller.ts
+++ apps/app/src/features/cleaning/controller/cleaning.controller.ts
@@ -1,20 +1,43 @@
 import { Controller, Delete, Param, ParseUUIDPipe } from '@nestjs/common';
-import { ApiExcludeController } from '@nestjs/swagger';
+import { ApiExcludeEndpoint, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 
 import { CleaningService } from '../cleaning.service';
 
-@ApiExcludeController()
 @Controller('testing')
 export class CleaningController {
   constructor(private readonly cleaningService: CleaningService) {}
 
+  @ApiExcludeEndpoint()
   @Delete('all-data')
   async deleteAllData() {
     await this.cleaningService.cleanDB();
   }
 
-  @Delete(':id')
-  async deleteUserById(@Param('id', ParseUUIDPipe) userId: string) {
-    await this.cleaningService.deleteUserById({ id: userId });
+  @ApiOperation({ summary: 'Удалить пользователя, его профиль и сессии' })
+  @ApiParam({
+    name: 'userId',
+    type: String,
+    description: 'UUID пользователя для удаления',
+    required: true,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Пользователь успешно удален',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Неправильный UUID пользователя',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'что то не так',
+  })
+  @Delete(':userId')
+  async deleteUserById(@Param('userId', ParseUUIDPipe) userId: string) {
+    try {
+      await this.cleaningService.deleteUserById({ id: userId });
+    } catch (e) {
+      throw new Error(e);
+    }
   }
 }
