f24dcbedde0409a1a76409bc5036637cc5740cbd
Vladislav Evg <linesgreen@mail.ru>
2024-09-22 18:05:05 +0300

    (IN-95)FEAT: add file upload module

--- apps/files/src/features/files/controller/file-upload.controller.ts
+++ apps/files/src/features/files/controller/file-upload.controller.ts
@@ -4,20 +4,30 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { FileUploadService } from '../applications/file-upload.service';
 import { ImageStorageAdapter } from '../../../common/adapters/image.storage.adapter';
 import { FileValidationPipe } from '../file-validation.pipe';
+import { diskStorage } from '../../multer-config';
 
 @Controller('upload')
 export class FileUploadController {
   constructor(
     private readonly fileUploadService: FileUploadService,
-    private readonly imageStorageAdapter: ImageStorageAdapter,
+    private readonly s3StorageAdapter: ImageStorageAdapter,
   ) {}
 
   @Post()
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage,
+    }),
+  )
   async uploadFile(@UploadedFile(FileValidationPipe) file: Express.Multer.File) {
-    const fileStream = await this.fileUploadService.createFileStream(file.path);
-    const result = await this.imageStorageAdapter.saveImageFromStream(fileStream);
-    await this.fileUploadService.deleteFile(file.path);
-    return result;
+    try {
+      const fileStream = await this.fileUploadService.createFileStream(file.path);
+      const result = await this.s3StorageAdapter.saveImageFromStream(fileStream);
+      await this.fileUploadService.deleteFile(file.path);
+      return result;
+    } catch (error) {
+      await this.fileUploadService.deleteFile(file.path);
+      throw error;
+    }
   }
 }
